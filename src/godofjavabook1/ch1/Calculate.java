a4e92d8594a28498a924c2b1c42b5cfa92d66227
truelightwater <truelightwater@gmail.com>
2023-01-12 19:37:38 +0900

    Chapter1 completed

--- src/godofjavabook1/ch1/Calculate.java
+++ src/godofjavabook1/ch1/Calculate.java
@@ -13,28 +13,24 @@ public class Calculate {
   public int add(int num1, int num2) {
     int sum = 0;
     sum = num1 + num2;
-
     return sum;
   }
 
   public int subtract(int num1, int num2) {
     int sum = 0;
     sum = num1 - num2;
-
     return sum;
   }
 
   public int multiply(int num1, int num2) {
     int sum = 0;
     sum = num1 * num2;
-
     return sum;
   }
 
   public int divide(int num1, int num2) {
     int sum = 0;
     sum = num1 / num2;
-
     return sum;
   }
 }

f02653d0a1758c589d732502d3bf8eecebc5bef3
truelightwater <truelightwater@gmail.com>
2023-01-12 19:36:19 +0900

    Chapter2 completed

--- src/godofjavabook1/ch2/Profile.java
+++ src/godofjavabook1/ch2/Profile.java
@@ -2,7 +2,9 @@ package godofjavabook1.ch2;
 
 public class Profile {
   public static void main(String[] args) {
-    System.out.println("My name is Truelightwater !");
-    System.out.println("My age is 33");
+    String name = "Truelightwater";
+    int age = 33;
+    System.out.println("My name is " +name);
+    System.out.println("My age is " +age);
   }
 }

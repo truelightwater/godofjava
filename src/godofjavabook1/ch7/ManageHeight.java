db30abb61e2ff1f5b0d951c670c2528f75570320
truelightwater <truelightwater@gmail.com>
2023-01-15 20:01:26 +0900

    Chapter7 completed

---
+++ src/godofjavabook1/ch7/ManageHeight.java
@@ -0,0 +1,56 @@
+package godofjavabook1.ch7;
+
+public class ManageHeight {
+  int[][] gradeHeights;
+
+  public static void main(String[] args) {
+    ManageHeight manager = new ManageHeight();
+    manager.setData();
+
+    for (int loop = 1; loop <= 5; loop++) {
+      manager.printHeight(loop);
+    }
+
+    System.out.println();
+
+    int loop = 1;
+    while (loop <=5) {
+      manager.printAverage(loop);
+      loop++;
+    }
+  }
+
+  public void setData() {
+    gradeHeights = new int[][]{
+        {170, 180, 173, 175, 177},
+        {160, 165, 167, 186},
+        {158, 177, 187, 176},
+        {173, 182, 181},
+        {170, 180, 165, 177, 172},
+    };
+  }
+
+  public void printHeight(int classNo) {
+    System.out.println("Class No.: " + classNo);
+    for (int studentHeight : gradeHeights[classNo - 1]) {
+      System.out.print(studentHeight+ " ");
+    }
+    System.out.println();
+  }
+
+  public void printAverage(int classNo) {
+    double sumHeight = 0.0;
+    double averageHeight = 0.0;
+
+    System.out.println("Class No.:" + classNo);
+    for (int studentHeight : gradeHeights[classNo - 1]) {
+      sumHeight += studentHeight;
+      averageHeight = sumHeight / gradeHeights[classNo - 1].length;
+    }
+
+    System.out.println("Height average : " +averageHeight);
+
+  }
+
+
+}
---
+++ src/godofjavabook1/example/ArrayMain.java
@@ -0,0 +1,11 @@
+package godofjavabook1.example;
+
+public class ArrayMain {
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      for (String arg : args) {
+        System.out.println(arg);
+      }
+    }
+  }
+}

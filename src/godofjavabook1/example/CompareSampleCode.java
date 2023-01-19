fd2586fdb46e0d22cf8fff6d38d0a63a43e05349
truelightwater <truelightwater@gmail.com>
2023-01-19 14:33:58 +0900

    equals() method vs ==

--- src/godofjavabook1/example/CompareSampleCode.java
+++ src/godofjavabook1/example/CompareSampleCode.java
@@ -4,6 +4,8 @@ public class CompareSampleCode {
   public static void main(String[] args) {
     String s1 = "apple";
     String s2 = new String("apple");
+    String s3 = new String("apple");
+    String s4 = "apple";
 
     if(s1 == s2) {
       System.out.println("두개의 값이 같음.");
@@ -11,8 +13,20 @@ public class CompareSampleCode {
       System.out.println("두개의 값이 같지 않음.");
     }
 
+    if (s2 == s3) {
+      System.out.println("두개의 값이 같음."); // 다만, hashCode()가 다르다.
+    } else {
+      System.out.println("두개의 값이 같지 않음.");
+    }
+
     if (s1.equals(s2)) {
-      System.out.println("두 개의 값이 같음.");
+      System.out.println("두개의 값이 같음.");
+    } else {
+      System.out.println("두개의 값이 같지 않음.");
+    }
+
+    if (s1 == s4) {
+      System.out.println("두개의 값이 같음.");
     } else {
       System.out.println("두개의 값이 같지 않음.");
     }
